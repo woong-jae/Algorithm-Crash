015fbf8eac7cdd5372076f1082b9e650216b649d
Johoseong <whghtjd320@naver.com>
2022-04-16 20:13:59 +0900

    Update 64065.py

--- Programmers/[64065] 튜플/Johoseong/64065.py
+++ Programmers/[64065] 튜플/Johoseong/64065.py
@@ -4,8 +4,9 @@ def solution(s):
 
     tuples.sort(key = lambda x:len(x)) # 길이로 오름차순 정렬
     
-    for i in range(len(tuples)):
-        tuple_set = tuples[i].split(',')
+    for t in tuples:
+        tuple_set = t.split(',')
+
         for j in tuple_set: # answer에 없는 원소 나옴 -> answer 맨 뒤에 추가
             if j not in answer:
                 answer.append(j)
