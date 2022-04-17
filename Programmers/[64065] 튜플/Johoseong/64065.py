9ebc4470037737eaf27884b32b43a58a4b2eb7e8
Jo hoseong <62900140+Johoseong@users.noreply.github.com>
2022-04-17 21:17:51 +0900

    Update 64065.py

--- Programmers/[64065] 튜플/Johoseong/64065.py
+++ Programmers/[64065] 튜플/Johoseong/64065.py
@@ -4,12 +4,13 @@ def solution(s):
 
     tuples.sort(key = lambda x:len(x)) # 길이로 오름차순 정렬
     
-    for t in tuples:
-        tuple_set = t.split(',')
+    sets = []
+    for t in tuples:    # 문자열 -> 정수
+        sets.append(list(map(int, t.split(','))))
 
-        for j in tuple_set: # answer에 없는 원소 나옴 -> answer 맨 뒤에 추가
-            if j not in answer:
-                answer.append(j)
+    for s in sets:
+        for i in s: # answer에 없는 원소 나옴 -> answer 맨 뒤에 추가
+            if i not in answer:
+                answer.append(i)
 
-    answer = list(map(int, answer))
     return answer
