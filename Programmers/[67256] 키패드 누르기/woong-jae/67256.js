ddd13ce6b67609aeaf794695aec6b7392e2416e5
woong-jae <jwchung0828@gmail.com>
2022-04-25 10:21:51 +0900

    woong-jae Programmers 67256

--- Programmers/[67256] 키패드 누르기/woong-jae/67256.js
+++ Programmers/[67256] 키패드 누르기/woong-jae/67256.js
@@ -3,17 +3,17 @@ function solution(numbers, hand) {
     
     let leftHand = 10, rightHand = 12;
     numbers.forEach(num => {
-        if(num === 0) num = 11;
+        if(num === 0) num = 11; // 0을 11로 처리
         
         if(num % 3 === 1) { // 1, 4, 7
             answer += "L"
             leftHand = num;
         }
-        else if(num % 3 === 0) { 
+        else if(num % 3 === 0) { // 3, 6, 9
             answer += "R"
             rightHand = num;
         }
-        else {
+        else { // 2, 5, 8, 0
             let leftDistance = Math.abs(num - (leftHand % 3 === 1 ? leftHand + 1 : leftHand)) / 3;
             if(leftHand % 3 === 1) leftDistance++;
             let rightDistance = Math.abs(num - (rightHand % 3 === 0 ? rightHand - 1 : rightHand)) / 3;
