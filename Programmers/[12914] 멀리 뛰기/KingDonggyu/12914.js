de9be14a916e19d26c30dd58bc1bd14936b61b6d
KingDonggyu <dgkim69166916@gmail.com>
2022-06-21 17:23:26 +0900

    KingDonggyu Programmers 12914

---
+++ Programmers/[12914] 멀리 뛰기/KingDonggyu/12914.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  const dp = [null, 1, 2];
+
+  if (n <= 2) {
+    return dp[n] % 1234567;
+  }
+
+  for (let x = 3; x <= n; x++) {
+    dp[x] = (dp[x - 1] + dp[x - 2]) % 1234567;
+  }
+
+  return dp[n];
+}
