1c0e2a3d5d34bc139df7af42e73ca4ec3a408125
SeongukBaek <bsu6235@gmail.com>
2022-09-21 19:58:19 +0900

    SUbbb: [1562] 계단 수

--- BOJ/[1562] 계단 수/SUbbb/1562.java
+++ BOJ/[1562] 계단 수/SUbbb/1562.java
@@ -1,48 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int[] numbers;
+class Main {
+    static long[][][] dp;
+    static int mod = 1000000000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int S = Integer.parseInt(br.readLine());
+        // 100, 10, 1024
+        dp = new long[N + 1][11][1<<10];
 
-        for (int i = 0; i < N; i++) {
-            if (S == 0) break;
+        for (int i = 1; i < 10; i++) dp[1][i][1 << i] = 1;
 
-            int maxIdx = findMax(i, i + S);
+        for (int i = 2; i < N + 1; i++) {
+            for (int end = 0; end < 10; end++) {
+                for (int k = 0; k < 1024; k++) {
+                    int b = k | (1 << end);
 
-            for (int j = maxIdx; j > i; j--) {
-                swap(j - 1, j);
-                S--;
+                    if (end == 0) {
+                        dp[i][end][b] = (dp[i][end][b] + dp[i - 1][end + 1][k]) % mod;
+                    } else if (end == 9) {
+                        dp[i][end][b] = (dp[i][end][b] + dp[i - 1][end - 1][k]) % mod;
+                    } else {
+                        dp[i][end][b] = (dp[i][end][b] + dp[i - 1][end + 1][k] + dp[i - 1][end - 1][k]) % mod;
+                    }
+                }
             }
         }
 
-        for (int n : numbers)
-            System.out.print(n + " ");
-    }
-
-    private static int findMax(int s, int e) {
-        int maxIdx = 0;
-        int max = 0;
-
-        for (int i = s; i <= e && i < numbers.length; i++) {
-            if (max < numbers[i]) {
-                maxIdx = i;
-                max = numbers[i];
-            }
+        long answer = 0;
+        for (int i = 0; i < 10; i++) {
+            answer = (answer + dp[N][i][1023]) % mod;
         }
 
-        return maxIdx;
-    }
-
-    private static void swap(int i1, int i2) {
-        int tmp = numbers[i1];
-        numbers[i1] = numbers[i2];
-        numbers[i2] = tmp;
+        System.out.println(answer);
     }
 }
